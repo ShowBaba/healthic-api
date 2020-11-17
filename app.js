90c6aa85c3c89fe7abfad1a8a3a5797bf2e60742
ShowBaba <samwise858@gmail.com>
2020-11-17 23:00:08 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const passport = require('passport');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+dotenv.config();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+const corsOptions = {
+  origin: '*',
+};
+app.use(cors(corsOptions));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+
+app.get('/', (req, res) => {
+  res.redirect('/api/v1');
+});
+
+app.use('/api/v1', indexRouter);
+app.use('/api/v1/users', usersRouter);
+
+// set up a wildcard route
+app.get('*', (req, res) => {
+  res.redirect('/api/v1');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const localUrl = config.mongoUrl;
+// eslint-disable-next-line no-unused-vars
+const liveUrl = process.env.DB_CONNECTION;
+// replace liveUrl with localUrl to use local mongodb
+const connect = mongoose.connect(liveUrl, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+// establish connection
+connect.then(
+  // eslint-disable-next-line no-unused-vars
+  (db) => {
+    console.log('Connected to Database');
+  },
+  (err) => {
+    console.log(err);
+  },
+);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/model.user.js
@@ -0,0 +1,25 @@
+/* eslint-disable linebreak-style */
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const User = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', User);
