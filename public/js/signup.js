a0ac4924596115362c00cb40fe34c72bc51ba5f5
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2020-12-17 11:55:27 +0100

    Separated form functionality and added feedback messages

---
+++ public/js/signup.js
@@ -0,0 +1,59 @@
+const formElement = document.querySelector('form');
+
+const validatePasswords = () => {
+    const passwordInputs = [
+        ...document.querySelectorAll('input[type="password"]')
+    ];
+    return passwordInputs[0].value === passwordInputs[1].value;
+};
+
+const validateFormInputs = async (event) => {
+    event.preventDefault();
+
+    const formInputs = [...document.getElementsByClassName('validate')];
+    const inputsValidated = formInputs.every((formInput) =>
+        formInput.checkValidity()
+    );
+    if (inputsValidated && validatePasswords()) {
+        const form = event.target;
+        const endpoint = form.action;
+        const formData = new FormData(form);
+
+        const user = {};
+        formData.forEach((value, key) => {
+            user[key] = value;
+        });
+
+        delete user.terms;
+        delete user['confirm-password'];
+        try {
+            const response = await fetch(endpoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8'
+                },
+                body: JSON.stringify(user)
+            });
+
+            const jsonResponse = await response.json();
+
+            const feedbackMessageElement = document.querySelector('.feedback-message');
+
+            if (jsonResponse.success) {
+                if (feedbackMessageElement.classList.contains('display-block')) {
+                    feedbackMessageElement.classList.remove('display-block');
+                }
+             else {
+                feedbackMessageElement.classList.add('display-block');
+            }
+                
+                setTimeout(() => window.location.href =
+                    'https://sokoto-gudu-5fb7c8add85c50070e7c411c.simplifyd.app/login', 3000);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+formElement.addEventListener('submit', validateFormInputs);
